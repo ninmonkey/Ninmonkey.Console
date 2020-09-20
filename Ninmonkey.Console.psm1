f356b278418827b8fd7425b51378d407cb959d38
Jake Bolton <ninmonkeys@gmail.com>
2020-09-20 18:41:06 -0500

    initial module structure

---
+++ Ninmonkey.Console.psm1
@@ -0,0 +1,28 @@
+﻿$private = @(
+
+)
+
+foreach ($file in $private) {
+    . ("{0}\private\{1}.ps1" -f $psscriptroot, $file)
+}
+
+$public = @(
+    'Format-Hashtable'
+)
+
+foreach ($file in $public) {
+    . ("{0}\public\{1}.ps1" -f $psscriptroot, $file)
+}
+
+# $completer = @(
+#     'Completer-dotnet'
+# )
+
+# foreach ($file in $completer) {
+#     . ("{0}\public\completer\{1}.ps1" -f $psscriptroot, $completer)
+# }
+
+$functionsToExport = @(
+    'Format-Hashtable'
+)
+Export-ModuleMember -Function $functionsToExport
---
+++ public/Format-Hashtable.ps1
@@ -0,0 +1,85 @@
+﻿function Format-HashTable {
+    <#
+    .synopsis
+        consistently format hashtables/PSCO objects
+    .example
+        > @{ name = 'Jack'; species = 'Cat'; age = 12 } | Format-HashTable -SingleLine
+        > @{ name = 'Jack'; species = 'Cat'; age = 12 } | Format-HashTable
+
+        @{species=Cat; age=12; name=Jack}
+        @{species=Cat; age=12; name=Jack}
+    .notes
+        todo: [1]
+            summarize types
+
+                PS> $hash = @{app='pip'; meta=@{a=3} }
+                PS> $hash | Format-HashTable
+
+            should return
+                @{meta=[Hashtable]; app=pip}
+
+        todo: [2] nested formattting
+            - [param]: -OutString:
+                wraps call with $stuff | out-string
+                before hitting out-debug or other streams
+
+
+            nested summary
+                PS> $hash = @{app='pip'; meta=@{a=3} }
+                PS> $hash | Format-HashTable
+
+            [a] should return
+                @{meta=@{a=3}; app=pip}
+            [b] or
+                @{
+                    meta = @{ a = 3 }
+                    app  = pip
+                }
+
+            [c] or
+
+                [Hashtable]:
+                    meta = @{a=3}
+                    app  = pip
+
+    #>
+    [CmdletBinding(DefaultParameterSetName = "FromPipe")]
+    param (
+        [Alias('Format')]
+        [Parameter(
+            ParameterSetName = "FromPipe",
+            Mandatory = $false,
+            Position = 0, HelpMessage = "format as line or format-pair"
+        )]
+        [ValidateSet('SingleLine', 'Table', 'Pair')]
+        [string]$FormatMode,
+
+        [Parameter(
+            ParameterSetName = "FromPipe",
+            Mandatory, ValueFromPipeline, HelpMessage = 'Object'
+        )]
+        [hashtable]$InputHash
+    )
+
+    process {
+        Switch ($FormatMode) {
+            'Pair' {
+                Write-Warning 'Piping to Format-Pair'
+                $InputHash | Format-Pair
+                break
+            }
+            'Table' {
+                $InputHash | Format-Table
+                break
+            }
+            'SingleLine' {
+                "$([pscustomobject]$InputHash)"
+                break
+            }
+            default {
+                "$([pscustomobject]$InputHash)"
+                break
+            }
+        }
+    }
+}
