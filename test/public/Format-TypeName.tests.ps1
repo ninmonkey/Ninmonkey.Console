08eaa957778611c2fddb852b0e018351f31693bf
Jake Bolton <ninmonkeys@gmail.com>
2020-10-21 09:41:55 -0500

    Cleaned up test

--- test/public/Format-TypeName.tests.ps1
+++ test/public/Format-TypeName.tests.ps1
@@ -35,4 +35,22 @@ Describe "Format-TypeName" {
         $file.GetType() | Format-TypeName -IgnorePrefix 'System.IO'
         | Should -Be 'DirectoryInfo'
     }
+
+    It 'Pipe: Types as text' {
+        # $inputList = 'b.system.bar', 'Int32', 'foo', 'Object[]', 'foo[]'
+        $inputList = 'b.system.bar', (23).GetType(), 'System.foo', 'System.Object[]', 'foo[]'
+        $expected = 'b.system.bar', 'Int32', 'foo', 'Object[]', 'foo[]'
+        $inputList | Format-TypeName
+        | Should -Be $expected
+    }
+
+    It 'Pipe: Types as text -WithBrackets' {
+        # $inputList = 'b.system.bar', 'Int32', 'foo', 'Object[]', 'foo[]'
+        $inputList = 'b.system.bar', (23).GetType(), 'System.foo', 'System.Object[]', 'foo[]'
+        $expected = '[b.system.bar]', '[Int32]', '[foo]', '[Object[]]', '[foo[]]'
+        $inputList | Format-TypeName -WithBrackets
+        | Should -Be $expected
+    }
+
+    # future: mixed type info and strings
 }
