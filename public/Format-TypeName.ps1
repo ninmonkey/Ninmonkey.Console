fbe78c609691dbb55982de5383fea8844b8bb1ec
Jake Bolton <ninmonkeys@gmail.com>
2020-10-22 18:02:19 -0500

    FormatTypeName - added sketch for humanizing templated names new: StringTo-Object

--- public/Format-TypeName.ps1
+++ public/Format-TypeName.ps1
@@ -1,4 +1,5 @@
-﻿
+﻿# $PSDefaultParameterValues["Format-TypeName:WithBrackets"] = $true
+
 function Format-TypeName {
     <#
     .synopsis
@@ -9,6 +10,7 @@ function Format-TypeName {
         PS> $list | %{ $_.GetType().FullName()} | AbbrFullName
     .notes
         todo:
+            - [ ] include full assembly name etc if wanted
             - [ ] a new param, prefixes to substitute
                 ex:
                 [System.Text.Json] => [t.Json]
@@ -36,7 +38,16 @@ function Format-TypeName {
 
         [Parameter(
             HelpMessage = "A List of Namespaces or prefixes to ignore")]
-        [string[]]$IgnorePrefix = @('System.Text', 'System.Management.Automation'),
+        [string[]]$IgnorePrefix = @(
+            # Todo: the easiest way to get past collisions is to sort this list by length before doing replacements
+            # that also removes the hard-coded 'system' removal
+            'System.Collections'
+            'System.Collections.Generic'
+            'System.Text'
+            'System.Management.Automation'
+            'System.Runtime.CompilerServices'
+
+        ),
 
         [Parameter(HelpMessage = "Output surrounded with '[]'")]
         [switch]$WithBrackets,
@@ -92,3 +103,101 @@ function Format-TypeName {
     }
 
 }
+
+
+function _FormatCommandInfo-GenericParameterTypeName {
+    param(
+        [Parameter(
+            ParameterSetName = "paramTypeAsInstance",
+            ValueFromPipeline,
+            HelpMessage = 'list of types')]
+        [System.Reflection.TypeInfo]$TypeInstance
+    )
+    Write-Warning 'this assumes type is a generic'
+    hr 2
+    $gcmLs = Get-Command Get-ChildItem
+    $gcmLs.Parameters.gettype() | Format-TypeName -WithBrackets
+
+    hr
+    $gcmLs.Parameters.GetType()
+    | Select-Object Name, FullName, Namespace, GenericParameterAttributes, GenericParameterPosition, GenericTypeArguments
+
+    $gcmLs.Parameters.GetType()
+    | Select-Object Name, NameSpace, GenericTypeArguments, @{
+        Name       = 'GenericType'
+        Expression = {
+            $_ | Format-TypeName
+        }
+    }
+
+
+    <# see also
+    IsConstructedGenericType   : True
+    IsGenericType              : True
+    IsGenericTypeDefinition    : False
+    IsGenericParameter         : False
+    IsTypeDefinition           : False
+#>
+    $gcmLs.Parameters.GetType()
+    | ForEach-Object {
+        $finalName = @(
+            $_.Namespace
+            $_.Name
+        ) -join '.'
+        Write-Warning "finalName: $finalName"
+
+        $genericTypeArgList = $gcmLs.Parameters.gettype().GenericTypeArguments
+        | ForEach-Object {
+            # todo: like above, no full name
+            $_.FullName  | Format-TypeName -WithBrackets
+        }
+
+        $finalGenericName = @(
+            $_.Namespace
+            $_.Name
+        ) -join '.'
+        $finalGenericName += '[wip, bar]'
+
+        Write-Warning "finalGenericName0: $finalGenericName"
+
+        $finalGenericName2 = @(
+            $_.Namespace
+            $_.Name
+        ) -join '.'
+        $finalGenericName += ( '[{0}]' -f $genericTypeArgList -join ', ' )
+
+        Write-Warning "finalGenericName1: $finalGenericName"
+        Write-Warning "finalGenericName2: $finalGenericName2"
+
+        $finalGenericName3 = '{0}[{1}]' -f @(
+            ( $finalGenericName2 | Format-TypeName )
+            $genericTypeArgList -join ''
+        )
+
+        Write-Warning "finalGenericName3: $finalGenericName3"
+
+        $meta = [ordered]@{
+            BaseType         = $_.BaseType | Format-TypeName -WithBrackets
+            NameAndSpace     = $_.Namespace, $_.Name -join '.'
+            Name             = $_.Name
+            NameSpace        = $_.Namespace
+            MemberType       = $_.MemberType
+            NameSpaceAbbr    = $_.Namespace | Format-TypeName  -WithBrackets
+            MemberTypeAbbr   = $_.MemberType | Format-TypeName  -WithBrackets
+            CustomAttributes = $_.CustomAttributes
+            GenericTypeArgs  = $genericTypeArgList
+        }
+        $meta['finalName'] = $finalName
+        $meta['finalGenericName'] = $finalGenericName3
+        [pscustomobject]$meta
+    }
+    | Format-List
+
+
+    'System.Collections.Generic' | Format-TypeName -WithBrackets
+    | Should -Be '[Generic]'
+
+    Write-Warning "Left off here. trigger when to check for generic names"
+}
+
+_FormatCommandInfo-GenericParameterTypeName
