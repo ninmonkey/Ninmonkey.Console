c2e300bfe630e351f7d88a1c0eb9bac59bd6a0dc
Jake Bolton <ninmonkeys@gmail.com>
2020-10-29 15:41:36 -0500

    New: Format-NullText

---
+++ test/public/Format-NullText.ps1
@@ -0,0 +1,43 @@
+﻿
+function Format-NullText {
+    <#
+    .synopsis
+        format null values to be human readable
+    .description
+        turns $null values, and null unicode in strings
+    #>
+    param(
+        [Parameter(
+            Mandatory, Position = 0,
+            ValueFromPipeline, HelpMessage = "piped objects, possible null")]
+        [AllowNull()]
+        # Not actually text, but,
+        [object]$InputObject,
+
+        [Parameter(
+            Position = 1,
+            HelpMessage = "String to use if not the default")]
+        [string]$ReplacementString = '␀'
+    )
+    begin {
+        $Uni = @{
+            Null       = "`u{0}"
+            NullSymbol = '␀'
+        }
+
+    }
+
+    process {
+        if ($null -eq $InputObject) {
+            return $Uni.NullSymbol
+        }
+
+        if ($InputObject -is 'String') {
+            $FilteredString = $InputObject -replace $uni.Null, $uni.NullSymbol
+            $FilteredString
+            return
+        }
+
+        return $InputObject
+    }
+}
---
+++ test/public/Format-NullText.tests.ps1
@@ -0,0 +1,45 @@
+﻿BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+<#
+add:
+
+if ($true) {
+    # need to test: should test
+    # 'System.System.System' | Format-TypeName -IgnorePrefix 'System' -ea Continue
+    'system.text'.GetType() | Format-TypeName
+
+}
+#>
+
+Describe "Format-NullText" -tags 'Console_Output', 'wip' {
+    Context 'Single Values' {
+        BeforeAll {
+            $Uni = {
+                Null       = "`u{0}"
+                NullSymbol = '␀'
+            }
+        }
+        It 'Return Int' {
+            10 | Format-NullText | Should -be 10
+        }
+
+        It 'Return Int' {
+            $Expected = 10, '', " ", $Uni.NullSymbol, $Uni.NullSymbol
+            $Values = 10, '', " ", $null, $Uni.Null
+            $Values | Format-NullText
+            | Should -be $Expected
+        }
+
+
+
+
+        # | Format-Table
+
+        # $null | Format-NullText
+        # , $null | Format-NullText
+        # Format-NullText $null
+    }
+
+}
