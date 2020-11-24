f563732cb75b96fe5701487278240482aed31b1a
Jake Bolton <ninmonkeys@gmail.com>
2020-11-24 09:16:19 -0600

    new: EncodingArgumentCompleter, EncodingArgumentConverterAttribute

--- private/seeminglySci/seeminglySci_import.ps1
+++ private/seeminglySci/seeminglySci_import.ps1
@@ -6,6 +6,10 @@ using namespace System.Text
 using namespace System.Management.Automation.Language
 using namespace System
 
+<#
+see also:
+    <https://github.com/SeeminglyScience/dotfiles/blob/a7a9bcf3624efe5be4988922ba2e35e8ff2fcfd8/PowerShell%2FUtility.psm1#L121>
+#>
 
 # [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '', Target = '??')]
 param()
@@ -140,6 +144,96 @@ $script:WellKnownNumericTypes = [type[]](
     [double],
     [decimal],
     [bigint])
+
+
+
+class EncodingArgumentCompleter : IArgumentCompleter {
+    hidden static [string[]] $s_encodings
+
+    static EncodingArgumentCompleter() {
+        $allEncodings = [Encoding]::GetEncodings()
+        $names = [string[]]::new($allEncodings.Length + 7)
+        $names[0] = 'ASCII'
+        $names[1] = 'BigEndianUnicode'
+        $names[2] = 'Default'
+        $names[3] = 'Unicode'
+        $names[4] = 'UTF32'
+        $names[5] = 'UTF7'
+        $names[6] = 'UTF8'
+
+        for ($i = 0; $i -lt $allEncodings.Length; $i++) {
+            $names[$i + 7] = $allEncodings[$i].Name
+        }
+
+        [EncodingArgumentCompleter]::s_encodings = $names
+    }
+
+    [IEnumerable[CompletionResult]] CompleteArgument(
+        [string] $commandName,
+        [string] $parameterName,
+        [string] $wordToComplete,
+        [CommandAst] $commandAst,
+        [IDictionary] $fakeBoundParameters) {
+        $results = [List[CompletionResult]]::new(<# capacity: #> 4)
+        foreach ($name in $this::s_encodings) {
+            if ($name -notlike "$wordToComplete*") {
+                continue
+            }
+
+            $results.Add(
+                [CompletionResult]::new(
+                    <# completionText: #> $name,
+                    <# listItemText:   #> $name,
+                    <# resultType:     #> [CompletionResultType]::ParameterValue,
+                    <# toolTip:        #> $name))
+        }
+
+        return $results.ToArray()
+    }
+}
+
+class EncodingArgumentConverterAttribute : ArgumentTransformationAttribute {
+    [object] Transform([EngineIntrinsics] $engineIntrinsics, [object] $inputData) {
+        if ($null -eq $inputData) {
+            return $null
+        }
+
+        if ($inputData -is [Encoding]) {
+            return $inputData
+        }
+
+        $convertedValue = default([Encoding])
+        if ([LanguagePrimitives]::TryConvertTo($inputData, [Encoding], [ref] $convertedValue)) {
+            return $convertedValue
+        }
+
+        if ($inputData -isnot [string]) {
+            $inputData = $inputData -as [string]
+            if ([string]::IsNullOrEmpty($inputData)) {
+                return $null
+            }
+        }
+
+        switch ($inputData) {
+            ASCII { return [Encoding]::ASCII }
+            BigEndianUnicode { return [Encoding]::BigEndianUnicode }
+            'Default' { return [Encoding]::Default }
+            Unicode { return [Encoding]::Unicode }
+            UTF32 { return [Encoding]::UTF32 }
+            UTF7 { return [Encoding]::UTF7 }
+            UTF8 { return [Encoding]::UTF8 }
+        }
+
+        return [Encoding]::GetEncoding($inputData)
+    }
+}
+
+# Terrible dirty hack to get around using non-exported classes in some of the function
+# parameter blocks. Don't use this in a real module pls
+# $typeAccel = [ref].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+# $typeAccel::Add('EncodingArgumentConverterAttribute', [EncodingArgumentConverterAttribute])
+# $typeAccel::Add('EncodingArgumentConverter', [EncodingArgumentConverterAttribute])
+# $typeAccel::Add('EncodingArgumentCompleter', [EncodingArgumentCompleter])
 
 function ConvertTo-Number {
     [Alias('number')]
@@ -332,88 +426,6 @@ function Get-ElementName {
 #         }
 #     }
 # }
-
-
-class EncodingArgumentCompleter : IArgumentCompleter {
-    hidden static [string[]] $s_encodings
-
-    static EncodingArgumentCompleter() {
-        $allEncodings = [Encoding]::GetEncodings()
-        $names = [string[]]::new($allEncodings.Length + 7)
-        $names[0] = 'ASCII'
-        $names[1] = 'BigEndianUnicode'
-        $names[2] = 'Default'
-        $names[3] = 'Unicode'
-        $names[4] = 'UTF32'
-        $names[5] = 'UTF7'
-        $names[6] = 'UTF8'
-
-        for ($i = 0; $i -lt $allEncodings.Length; $i++) {
-            $names[$i + 7] = $allEncodings[$i].Name
-        }
-
-        [EncodingArgumentCompleter]::s_encodings = $names
-    }
-
-    [IEnumerable[CompletionResult]] CompleteArgument(
-        [string] $commandName,
-        [string] $parameterName,
-        [string] $wordToComplete,
-        [CommandAst] $commandAst,
-        [IDictionary] $fakeBoundParameters) {
-        $results = [List[CompletionResult]]::new(<# capacity: #> 4)
-        foreach ($name in $this::s_encodings) {
-            if ($name -notlike "$wordToComplete*") {
-                continue
-            }
-
-            $results.Add(
-                [CompletionResult]::new(
-                    <# completionText: #> $name,
-                    <# listItemText:   #> $name,
-                    <# resultType:     #> [CompletionResultType]::ParameterValue,
-                    <# toolTip:        #> $name))
-        }
-
-        return $results.ToArray()
-    }
-}
-
-class EncodingArgumentConverterAttribute : ArgumentTransformationAttribute {
-    [object] Transform([EngineIntrinsics] $engineIntrinsics, [object] $inputData) {
-        if ($null -eq $inputData) {
-            return $null
-        }
-
-        if ($inputData -is [Encoding]) {
-            return $inputData
-        }
-
-        $convertedValue = default([Encoding])
-        if ([LanguagePrimitives]::TryConvertTo($inputData, [Encoding], [ref] $convertedValue)) {
-            return $convertedValue
-        }
-
-        if ($inputData -isnot [string]) {
-            $inputData = $inputData -as [string]
-            if ([string]::IsNullOrEmpty($inputData)) {
-                return $null
-            }
-        }
-
-        switch ($inputData) {
-            ASCII { return [Encoding]::ASCII }
-            BigEndianUnicode { return [Encoding]::BigEndianUnicode }
-            'Default' { return [Encoding]::Default }
-            Unicode { return [Encoding]::Unicode }
-            UTF32 { return [Encoding]::UTF32 }
-            UTF7 { return [Encoding]::UTF7 }
-            UTF8 { return [Encoding]::UTF8 }
-        }
-
-        return [Encoding]::GetEncoding($inputData)
-    }
-}
 
 function Get-TypeDefaultValue {
     [Alias('default')]
