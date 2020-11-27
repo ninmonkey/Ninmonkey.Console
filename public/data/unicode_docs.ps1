2f99b0d0bbc6f273a6d21975a2b93b968cb2df2d
Jake Bolton <ninmonkeys@gmail.com>
2020-11-27 12:32:43 -0600

    Initial Unicode metadata

---
+++ public/data/unicode_docs.ps1
@@ -0,0 +1,21 @@
+﻿$_unicode_docs = @{
+    Main                      = 'https://docs.microsoft.com/en-us/dotnet/standard/base-types/character-encoding-introduction'
+
+    # counts [char]s, [rune]s, and [textElement] graphemes
+    Counting_GraphemeRuneChar = 'https://docs.microsoft.com/en-us/dotnet/standard/base-types/character-encoding-introduction#grapheme-clusters'
+
+    Surrogate_Pairs           = 'https://docs.microsoft.com/en-us/dotnet/standard/base-types/character-encoding-introduction#surrogate-pairs'
+
+    About_Rune                = 'https://docs.microsoft.com/en-us/dotnet/api/system.text.rune?view=net-5.0'
+
+    About_ControlCodes        = 'https://en.wikipedia.org/wiki/C0_and_C1_control_codes'
+
+    Unsorted_Docs             = @{
+        StringInfo                            = 'https://docs.microsoft.com/en-us/dotnet/api/system.globalization.stringinfo?view=net-5.0'
+
+        TextElement                           = 'https://docs.microsoft.com/en-us/dotnet/api/system.globalization.stringinfo.gettextelementenumerator?view=net-5.0'
+
+        'StringInfo.GetTextElementEnumerator' = 'https://docs.microsoft.com/en-us/dotnet/api/system.globalization.stringinfo.gettextelementenumerator?view=net-5.0'
+    }
+
+}
---
+++ public/data/unicode_metadata.ps1
@@ -0,0 +1,54 @@
+﻿
+# missing (utfbom/non-bom and pwsh specific)
+
+
+<#
+this is not to replace unicode data and code ranges,
+rather to suppliment or summarize important info
+#>
+$_unicode_metadata = @{
+    Docs              = @{
+    }
+    'MaxValue'        = 0x10ffff
+
+    'Ranges'          = @{
+        'C0 Control Codes'        = @{
+            First = 0x00
+            Last  = 0x1f
+        }
+        'C1 Control Codes'        = @{
+            First = 0x80
+            Last  = 0x9f
+        }
+        'C0 Control Char Symbols' = @{
+            First = 0x2400
+            # technically space isn't control, but the symbol exists
+            #0x241f
+            Last  = 0x2f20
+        }
+
+        'SurrogatePairs'          = @{
+            First = 0xD800
+            Last  = 0xDFFF
+        }
+    }
+
+    'Named_Codepoint' = @{
+        'Null'                = 0x00
+        'Delete'              = 0x1f
+        'Control Char Symbol' = @{
+            'Null'   = 0x2400
+            'Delete' = 0x2421
+        }
+    }
+}
+
+$_unicode_web_query = @{
+
+    'Fileformat.info' = @{
+        # ex: 2400
+        'Codepoint' = 'https://www.fileformat.info/info/unicode/char/{{codepoint_hex}}/index.htm'
+        # ex: 'Cc'
+        'Category'  = 'https://www.fileformat.info/info/unicode/category/Cc/index.htm'
+    }
+}
