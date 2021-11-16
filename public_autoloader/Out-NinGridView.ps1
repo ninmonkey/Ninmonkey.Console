18f3ea9afe37cbf89da4086877caeb605fd0833f
Jake Bolton <ninmonkeys@gmail.com>
2021-11-16 10:38:40 -0600

    New: Out-NinGridView

---
+++ public_autoloader/Out-NinGridView.ps1
@@ -0,0 +1,56 @@
+#Requires -Version 7
+$publicToExport.function += @(
+    'Out-NinGridView'
+)
+$publicToExport.alias += @(
+    'Out-Grid'
+)
+function Out-NinGridView {
+    <#
+    .synopsis
+        Regular Out-GridView, except that it shows on top of windows
+    .description
+        next: test if -passthru blocks the window restore or not
+    .notes
+        .
+    .example
+        PS> Get-Processs | Out-NinGridView
+    #>
+    [Alias('Out-Grid')]
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, v)]
+        [object]$InputObject
+    )
+
+    begin {
+        $objList = [list[object]]::new()
+    }
+    process {
+        $InputObject | ForEach-Object {
+            $objList.Add( $_ )
+        }
+    }
+    end {
+        # Label 'status' '=> piping...'
+        $objList
+        | Out-GridView #-PassThru
+
+        # Label 'status' '=> restore'
+        if (Get-Command -ea ignore 'Window->Get') {
+
+            $w = Window->Get '*Out-GridView*'
+            $w | Minimize-Window
+            # Start-Sleep 0.1
+            $w | Restore-Window
+        }
+        # Label 'status' '=> Done'
+    }
+}
+
+# works
+if ($false) {
+    Get-ChildItem . | Out-NinGridView
+    Start-Sleep 0.3
+    Get-ChildItem . | Select-Object * | Out-NinGridView
+}
