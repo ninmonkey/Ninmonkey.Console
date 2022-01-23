0f15b228a648513eb3ff58b5c5375c8187b40175
Jake Bolton <ninmonkeys@gmail.com>
2022-01-23 14:54:29 -0600

    Hashtable: Optionally mutate left input

--- public_autoloader/Join-Hashtable.ps1
+++ public_autoloader/Join-Hashtable.ps1
@@ -96,6 +96,8 @@ Function Join-Hashtable {
         Copy and append BaseHash with new values from UpdateHash
     .notes
         future: add valuefrom pipeline to $UpdateHash param ?
+    .example
+        Join-Hashtable -Base $
     .link
         Ninmonkey.Console\Join-Hashtable
     .link
@@ -106,6 +108,7 @@ Function Join-Hashtable {
         PSScriptTools\Join-Hashtable
     #>
     [cmdletbinding()]
+    [outputType('System.Collections.Hashtable')]
     param(
         # base hashtable
         [Parameter(Mandatory, Position = 0)]
@@ -113,15 +116,27 @@ Function Join-Hashtable {
 
         # New values to append and/or overwrite
         [Parameter(Mandatory, Position = 1)]
-        [hashtable]$UpdateHash
+        [hashtable]$OtherHash,
+
+        # normal is to not modify left, return a new hashtable
+        [Parameter()][switch]$MutateLeft
+
+        # default Left wins if they share a key name
+        # [Parameter()][switch]$PrioritizeRight
     )
 
     # don't mutate $BaseHash
     process {
-        $NewHash = [hashtable]::new( $BaseHash )
-        $UpdateHash.GetEnumerator() | ForEach-Object {
-            $NewHash[ $_.Key ] = $_.Value
+        # $NewHash = [hashtable]::new( $BaseHash )
+        if (! $MutateLeft ) {
+            $TargetHash = [hashtable]::new( $BaseHash )
+        } else {
+            Write-Debug 'Mutate enabled'
+            $TargetHash = $BaseHash
         }
-        $NewHash
+        $OtherHash.GetEnumerator() | ForEach-Object {
+            $TargetHash[ $_.Key ] = $_.Value
+        }
+        $TargetHash
     }
 }
