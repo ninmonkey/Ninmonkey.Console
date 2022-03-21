43b2e9de8127d9b02e9c63b42e81875510ca7bc5
Jake Bolton <ninmonkeys@gmail.com>
2022-03-21 18:17:00 -0500

    Create Measure-ObjectCount.ps1
    New function: Count/Len

---
+++ public_autoloader/Measure-ObjectCount.ps1
@@ -0,0 +1,72 @@
+using namespace System.Collections.Generic
+#Requires -Version 7
+
+if ( $publicToExport ) {
+    $publicToExport.function += @(
+        'Measure-ObjectCount'
+
+    )
+    $publicToExport.alias += @(
+        'Count', 'Len'
+    )
+}
+
+function Measure-ObjectCount {
+    <#
+    .synopsis
+        Simple. Counts items. Shortcut for the cli, simplifed version of Dev.Nin\Measure-ObjectCount
+    .description
+       This is for cases where you had to use
+
+        PS> ... | Measure-Object | % Count | ...
+    .notes
+        future:
+            - Parameter <condition> that works how Sort-Object's -Property param works
+                for custom counting conditions?
+    .example
+        🐒> ls . | Count # how many files?
+        4
+
+        🐒> ClassExplorer\Find-Type *xml* | len
+
+            # prints
+            235
+    .outputs
+          [int]
+    .link
+        Dev.Nin\Where-IsNotBlank
+    .link
+        Ninmonkey.Console\Measure-ObjectCount
+
+    #>
+
+    [alias( 'Count', 'Len')]
+    [CmdletBinding()]
+    param(
+        #Input from the pipeline
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [object[]]$InputObject,
+
+        # do not count 'Blank' values
+        [Alias('IgnoreNull')]
+        [Parameter()][switch]$IgnoreBlank
+
+    )
+    begin {
+        $objectList = [List[object]]::new()
+    }
+    process {
+        $objectList.AddRange( $InputObject )
+
+    }
+    end {
+        if ($IgnoreBlank) {
+            $objectList
+            | Dev.Nin\Where-IsNotBlank
+            | Measure-Object | ForEach-Object Count
+        } else {
+            $objectList
+            | Measure-Object | ForEach-Object Count
+        }
+    }
+}
