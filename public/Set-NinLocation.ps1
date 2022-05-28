fd8ed6cfb73271d59615e49a6cda54ad180e3a28
Jake Bolton <ninmonkeys@gmail.com>
2022-05-28 17:19:47 -0500

    remove dead code

--- public/Set-NinLocation.ps1
+++ public/Set-NinLocation.ps1
@@ -13,6 +13,8 @@ function Set-NinLocation {
 
         # go back
         🐒> Goto -Back
+        🐒> Goto '-'    # normal cd history works too
+        🐒> Goto '+'
     .example
         🐒> Goto './folder'
             # runs: cd './folder'
@@ -28,11 +30,11 @@ function Set-NinLocation {
     .notes
         future
             - [ ] if exception, try resolve-VSCodePath
-            - [ ] if fail, then split path
     #>
 
     [Alias('Goto')]
-    [CmdletBinding(PositionalBinding = $false,
+    [CmdletBinding(
+        PositionalBinding = $false,
         DefaultParameterSetName = 'GoToPath')]
 
     param(
@@ -42,8 +44,7 @@ function Set-NinLocation {
         [Parameter(
             ParameterSetName = 'GoToPath',
             Mandatory, Position = 0,
-            ValueFromPipeline,
-            ValueFromPipelineByPropertyName)]
+            ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [Alias('PSPath')]
         [String]$Path,
 
@@ -93,7 +94,6 @@ function Set-NinLocation {
             $DestItem = Get-Item ($path | Split-Path )
         }
         $DestItem.PSPovider.Name | Join-String -op 'provider: ' | Write-Debug
-        # todo; maybe test that item is container, before throwing for others
         # see Test-IsDirectory
         if ($DestItem.PSProvider.Name -ne 'filesystem') {
             # I don't need to require FS,
@@ -123,63 +123,16 @@ function Set-NinLocation {
             $DestItem = $DestItem.Directory
         }
 
-        if ($true) {
+        Write-Debug "Moving to: $DestItem"
 
-            Write-Debug "Moving to: $DestItem"
-
-            Push-Location -Path $DestItem @stackname -PassThru
-            | Label 'push -> ' | Write-Information
+        Push-Location -Path $DestItem @stackname -PassThru
+        | Label 'push -> ' | Write-Information
 
 
-            if ($AlwaysLsAfter) {
-                Ninmonkey.Console\Get-NinChildItem
-            }
-            return
+        if ($AlwaysLsAfter) {
+            Ninmonkey.Console\Get-NinChildItem
         }
-        # if ($True) {
-        #     return
-        #     # Write-Debug 'go ->'
-        #     # Inspect-PathInfoStack
-        #     # Write-Debug "go-> '$DestItem'"
-
-        #     # Set-Location -StackName $null -PassThru | Label 'new stack' | Write-Information
-
-        #     # Push-Location -Path $DestItem -StackName $null -PassThru | Label 'new push' | Write-Information
-        #     # Get-Location -Stack | Label 'get-stack' | Write-Information
-
-
-
-
-        #     # This snippet, from in a module,  *will* set user's default stack to the new path
-        #     # the problem is that truncates it, so you only want to, if, you're not currently the current stack
-        #     # H1 'starting...'
-
-        #     # 0..4 | ForEach-Object {
-        #     #     Push-Location -Path (_randPath) -PassThru | Label 'push ->'
-        #     # }
-
-        #     # Inspect-PathInfoStack
-        #     # Push-Location -Path (Get-Item . ) -StackName $null -PassThru
-        #     # Push-Location -Path (Get-Item '~') -StackName $null -PassThru
-        #     # Get-Location -Stack
-
-        #     return
-        # }
-
-        # if ($false) {
-
-        #     H1 'stack?'
-        #     Get-Location -Stack
-        #     Hr
-        #     Get-Item . | Label -fg orange 'loc='
-        #     H1 'set-loc'
-        #     # this worked
-        #     Set-Location -StackName $null -PassThru | Label 'stack "nin"'
-        #     Get-Item . | Label -fg orange 'loc='
-        #     H1 'push-loc'
-        #     Push-Location -Path (Get-Item .) -PassThru -StackName $null | Label 'stack2'
-        #     Get-Item . | Label -fg orange 'loc='
-        # }
+        return
 
     }
 }
