03bdf06439d52d1ff3f9cd8fcefe4a74951af251
Jake Bolton <ninmonkeys@gmail.com>
2022-12-10 15:00:06 -0600

    restructure location of Join.* functions like Format-UnorderedList

--- public_autoloader/join.star/JoinStr.Format-UnorderedList.ps1
+++ public_autoloader/join.star/JoinStr.Format-UnorderedList.ps1
@@ -2,18 +2,14 @@
 
 if ( $publicToExport ) {
     $publicToExport.function += @(
-        'Format-ShortTypename'
-        # 'Format-UnorderedList'
-        'Format-ShortSciTypeName'
+        'Format-UnorderedList'
     )
     $publicToExport.alias += @(
-        'shortTypeName' # 'Format-ShortTypename'
-        # 'UL'
-        # 'join.UL'
-        'shortType' # 'Format-ShortSciTypeName
+        'UL'      # 'Format-UnorderedList'
+        # 'join.UL' # 'Format-UnorderedList'
     )
 }
-
+# Requires: Ninmonkey.Console\zeroDepend_autoloader\Format-ShortTypeName.ps1
 
 function Format-UnorderedList {
     <#
@@ -183,108 +179,108 @@ function Format-UnorderedList {
         # $Items | Join-String -sep "`n $StrBullet " -op "`n $StrBullet " -os "`n"
     }
 }
-$script:__moduleExists = @{ # cached for: Format-ShortSciTypeName
+$script:__moduleExists ??= @{
     'ClassExplorer' = [bool](Get-Module 'ClassExplorer')
 }
 
-function Format-ShortSciTypeName {
-    <#
-    .synopsis
-        Abbreviate  TypeNames from any [Object] or [Type] info
-    .EXAMPLE
-        PS> [System.Management.Automation.VerboseRecord] | shortTypeName
-        PS> gi env: | ShortType
-    .EXAMPLE
-        PS> Format-ShortSciTypeName -NoColor -InputObject (Get-Item env:)
+# function Format-ShortSciTypeName {
+#     <#
+#     .synopsis
+#         Abbreviate  TypeNames from any [Object] or [Type] info
+#     .EXAMPLE
+#         PS> [System.Management.Automation.VerboseRecord] | shortTypeName
+#         PS> gi env: | ShortType
+#     .EXAMPLE
+#         PS> Format-ShortSciTypeName -NoColor -InputObject (Get-Item env:)
 
-                [Dictionary<TKey, TValue>.ValueCollection]
+#                 [Dictionary<TKey, TValue>.ValueCollection]
 
-    .notes
-        as a fallback, use: <Format-ShortTypeName>. I didn't put a gcm test in there
+#     .notes
+#         as a fallback, use: <Format-ShortTypeName>. I didn't put a gcm test in there
 
-        requires <https://github.com/SeeminglyScience/ClassExplorer>
-        is assumed internal only
-        see: <https://github.com/SeeminglyScience/ClassExplorer/blob/a5aa12af456f6d10a233428460af0fbfbd0f24aa/src/ClassExplorer/Internal/_Format.cs>
+#         requires <https://github.com/SeeminglyScience/ClassExplorer>
+#         is assumed internal only
+#         see: <https://github.com/SeeminglyScience/ClassExplorer/blob/a5aa12af456f6d10a233428460af0fbfbd0f24aa/src/ClassExplorer/Internal/_Format.cs>
 
-    .link
-        https://github.com/SeeminglyScience/ClassExplorer
-    .link
-        Ninmonkey.Console\Format-ShortTypeName
-    #>
-    # [AssumeExperimental()] # attribute NYI
-    # [SupportsEnvNoColor()] # attribute NYI
-    # [RequiresModule('ClassExplorer')] # attribute NYI
-    [OutputType('System.String')]
-    [Alias('shortType')]
-    param(
-        # any object or typeinfo instance
-        [AllowNull()]
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [object]$InputObject,
-        [switch]$NoColor
-    )
-    process {
-        if ($null -eq $InputObject) {
-            return # $null?
-        }
-        if ($InputObject -is 'type') {
-            $Target = $InputObject
-        } else {
-            $Target = $InputObject.GetType()
-        }
-        if (! $script:__moduleExists['ClassExplorer'] ) {
-            $target | Format-ShortTypeName
-            return
-        }
-        $name = [ClassExplorer.Internal._Format]::Type($Target)
-        | Join-String -op '[' -os ']'
-        if ($Env:NO_COLOR -or $NoColor) {
-            return $Name | StripAnsi
-        }
-        return $Name
-    }
-}
+#     .link
+#         https://github.com/SeeminglyScience/ClassExplorer
+#     .link
+#         Ninmonkey.Console\Format-ShortTypeName
+#     #>
+#     # [AssumeExperimental()] # attribute NYI
+#     # [SupportsEnvNoColor()] # attribute NYI
+#     # [RequiresModule('ClassExplorer')] # attribute NYI
+#     [OutputType('System.String')]
+#     [Alias('shortType')]
+#     param(
+#         # any object or typeinfo instance
+#         [AllowNull()]
+#         [Parameter(Mandatory, ValueFromPipeline)]
+#         [object]$InputObject,
+#         [switch]$NoColor
+#     )
+#     process {
+#         if ($null -eq $InputObject) {
+#             return # $null?
+#         }
+#         if ($InputObject -is 'type') {
+#             $Target = $InputObject
+#         } else {
+#             $Target = $InputObject.GetType()
+#         }
+#         if (! $script:__moduleExists['ClassExplorer'] ) {
+#             $target | Format-ShortTypeName
+#             return
+#         }
+#         $name = [ClassExplorer.Internal._Format]::Type($Target)
+#         | Join-String -op '[' -os ']'
+#         if ($Env:NO_COLOR -or $NoColor) {
+#             return $Name | StripAnsi
+#         }
+#         return $Name
+#     }
+# }
 
-function Format-ShortTypeName {
-    <#
-    .EXAMPLE
-        PS> [System.Management.Automation.VerboseRecord] | shortTypeName
+# function Format-ShortTypeName {
+#     <#
+#     .EXAMPLE
+#         PS> [System.Management.Automation.VerboseRecord] | shortTypeName
 
-            [VerboseRecord]
-    .example
+#             [VerboseRecord]
+#     .example
 
-        PS>
-        @( Get-Date; Gi . ;
-            [System.Management.Automation.VerboseRecord]
-            (gi fg:\)
-        ) | ShortTypeName
+#         PS>
+#         @( Get-Date; Gi . ;
+#             [System.Management.Automation.VerboseRecord]
+#             (gi fg:\)
+#         ) | ShortTypeName
 
-            [DateTime]
-            [IO.DirectoryInfo]
-            [VerboseRecord]
-            [RgbColorProviderRoot]
-    .notes
-        render-* implies ansi colors
+#             [DateTime]
+#             [IO.DirectoryInfo]
+#             [VerboseRecord]
+#             [RgbColorProviderRoot]
+#     .notes
+#         render-* implies ansi colors
 
-    .link
-        Ninmonkey.Console\Format-ShortTypeName
-    .link
-        Ninmonkey.Console\Render-ShortTypeName
+#     .link
+#         Ninmonkey.Console\Format-ShortTypeName
+#     .link
+#         Ninmonkey.Console\Render-ShortTypeName
 
-    #>
-    [Alias('shortTypeName')]
-    param(
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [object]$InputObject
-    )
-    process {
-        if ($InputObject -is 'type') {
-            $Target = $InputObject
-        } else {
-            $Target = $InputObject.GetType()
-        }
+#     #>
+#     [Alias('shortTypeName')]
+#     param(
+#         [Parameter(Mandatory, ValueFromPipeline)]
+#         [object]$InputObject
+#     )
+#     process {
+#         if ($InputObject -is 'type') {
+#             $Target = $InputObject
+#         } else {
+#             $Target = $InputObject.GetType()
+#         }
 
-        $Target.FullName -replace '^System\.(Management.Automation\.)?', '' -replace '^PoshCode\.Pansies', 'Pansies'
-        | Join-String -op '[' -os ']'
-    }
-}
+#         $Target.FullName -replace '^System\.(Management.Automation\.)?', '' -replace '^PoshCode\.Pansies', 'Pansies'
+#         | Join-String -op '[' -os ']'
+#     }
+# }
