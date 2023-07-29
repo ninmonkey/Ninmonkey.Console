ebfd03936b7092e0dc20a68358535fb6d2726850
Jake Bolton <ninmonkeys@gmail.com>
2023-07-29 18:37:32 -0500

    misc updates

--- public_autoloader/Test-IsDirectory.ps1
+++ public_autoloader/Test-IsDirectory.ps1
@@ -6,18 +6,17 @@ $script:publicToExport.alias += @(
     'Test-IsDirectory'
 )
 
-
 function Test-IsContainer {
     <#c
     .Synopsis
-        Is it a directory, or other container type??'
+        Is it a directory, or other container type?'
     .example
         PS> Test-IsDirectory '.'
         # $True
 
         PS> Test-IsDirectory 'foo.png'
         # $False
-         Todo: see: <https://github.com/SeeminglyScience/dotfiles/blob/7e224fb5fc16998f5956aead620baa4ef9407ebd/PowerShell/Utility.psm1#L231>>
+        Todo: see: <https://github.com/SeeminglyScience/dotfiles/blob/7e224fb5fc16998f5956aead620baa4ef9407ebd/PowerShell/Utility.psm1#L231>>
             foreach ($item in $pathList) {
             $extraArgs = ($item = Get-Item $Path -ea 0) -and $item.PSIsContainer | ?? { '' } : { '-r' }
             & $code $pathList $extraArgs
@@ -42,30 +41,21 @@ function Test-IsContainer {
     begin {
     }
     process {
-        <#
-         #>
         $InputObject | ForEach-Object {
-            if ($Null -eq $_) {
-                return $false 
-            }
             $TargetItem = $_
+            if ($Null -eq $_) {
+                return $false
+            }
             $item = Get-Item $TargetItem -ea ignore
             if ( [string]::IsNullOrEmpty( $TargetItem )) {
-                return $false;
+                return $false
             }
             if ($null -eq $item) {
                 return $false
             }
             if ($IsContainer -and $Item.PSIsContainer) {
-                return $True;
+                return $True
             }
-            # try {
-            #     $item = Get-Item $TargetItem -ea stop
-            # } catch {
-            #     # definitely didn't exist
-            #     Write-Debug 'GI failed'
-            #     return $false;
-            # }
             $isType = $Item -is 'System.IO.DirectoryInfo'
             $meta = @{
                 IsDirInfo         = $isDir
